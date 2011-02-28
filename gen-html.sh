f3b574f398714f05418708835823ecd56ef324e5
robin <robin@c13897cc-ca07-0410-ab8c-73041ad34321>
2011-02-28 19:54:53 +0000

    * Quick hack bash script to generate some html to link to the RPMs
    git-svn-id: http://svn.riviera.org.uk/repo/RPMS@2892 c13897cc-ca07-0410-ab8c-73041ad34321

---
+++ gen-html.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+UrlBase="http://svn.riviera.org.uk/repo/RPMS"
+
+StarDir=`pwd`
+
+echo "<ul>"
+for Package in * ; do 
+
+	if [ -d $Package ] ; then
+		cd $Package
+		echo "<li>$Package</li><ul>"
+		for RPM in `find . -name \*.rpm` ; do
+			
+			tmp=$(echo $RPM | sed 's/^\.//g')
+			RPM=$tmp
+			
+			FileName=$(basename $RPM)
+			echo "<li><a href=\"${UrlBase}/${Package}${RPM}\">$FileName</a>"
+		done
+		echo "</ul>"
+	fi
+
+
+	cd $StarDir
+done
+echo "</ul>"
